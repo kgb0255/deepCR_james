c51ab94396d3293e6b2d6ef062ee0135f88a5025
James Kyubin Kwon <kgb0255@berkeley.edu>
2020-06-24 01:44:14 -0700

    structure change

--- get_dark.py
+++ get_dark.py
@@ -4,80 +4,92 @@ import os
 import numpy as np
 import sys
 
-def get_dark():
-	dark_lists = {}
-	raw_dark_dir = '/global/cfs/cdirs/desi/spectro/data/'
-	#recent_exps = [20200607,20200608,20200609]
-	recent_exps = os.listdir(raw_dark_dir)
-	length = len(recent_exps)
-	print('--------------------Searching for Dark Frames--------------------')
-	for i,date in enumerate(recent_exps):
-	    print(f'{i}/{length}', end = '\r')
-	    if str(date) != 'README.html':
-	        exp_ids = os.listdir(os.path.join(raw_dark_dir,str(date)))
-	        for exp_id in exp_ids:
-	            exp_dir = os.path.join(raw_dark_dir,str(date),str(exp_id))
-	            files = os.listdir(exp_dir)
-	            for f in files:
-	                if 'desi-' in f:
-	                    f_dir = os.path.join(exp_dir,f)
-	                    with fits.open(f_dir) as hdul:
-	                        try:
-	                            obstype = hdul[1].header['OBSTYPE']
-	                            if obstype == 'DARK':
-	                                dark_lists[exp_id] = f_dir
-	                        except:
-	                            pass
-	    else: continue
-	                        
-	np.save('/global/homes/k/kgb0255/packages/deepcr/dark_lists.npy', np.array(dark_lists))
+def get_raw(search_type):
+    '''
+    Get raw images 
 
-	return None
+    param search_type: obstype to retreive - either 'DARK' or 'SCIENCE'
+    '''
+    raw_lists = {}
+    raw_dir = '/global/cfs/cdirs/desi/spectro/data/'
+    #recent_exps = [20200607,20200608,20200609]
+    recent_exps = os.listdir(raw_dir)
+    length = len(recent_exps)
+    for i,date in enumerate(recent_exps):
+        print(f'{i}/{length}', end = '\r')
+        if str(date) != 'README.html':
+            exp_ids = os.listdir(os.path.join(raw_dir,str(date)))
+            for exp_id in exp_ids:
+                exp_dir = os.path.join(raw_dir,str(date),str(exp_id))
+                files = os.listdir(exp_dir)
+                for f in files:
+                    if 'desi-' in f:
+                        f_dir = os.path.join(exp_dir,f)
+                        with fits.open(f_dir) as hdul:
+                            try:
+                                obstype = hdul[1].header['OBSTYPE']
+                                if obstype == search_type:
+                                    raw_lists[exp_id] = f_dir
+                            except:
+                                pass
+        else: continue
+                            
+    np.save(f'/global/homes/k/kgb0255/packages/deepcr/raw_lists_{search_type}.npy', np.array(raw_lists))
 
-def get_dark_preproc():
-	preproc_dir = '/global/cfs/cdirs/desi/spectro/redux/andes/preproc/'
-	recent_exps = os.listdir(preproc_dir)
-	length = len(recent_exps)
+    return None
 
-	dark_lists = np.load('/global/homes/k/kgb0255/packages/deepcr/dark_lists.npy', allow_pickle = True)[()]
-	dark_ids = list(dark_lists.keys())
-	dark_preproc = {}
+def get_preproc():
+    '''
+    Cross search the preprocessed science images based on the raw sceince images.
 
-	print('\n----Matching existing dark frames with the preprocessed data-----')
-	for i,date in enumerate(recent_exps):
-		print(f'{i}/{length}', end = '\r')
-		exp_ids = os.listdir(os.path.join(preproc_dir,str(date)))
-		for exp_id in exp_ids:
-			if exp_id in dark_ids:
-				preproc_dir = os.path.join(preproc_dir,str(date),exp_id)
-				_dir = dark_lists[exp_id]
-				dark_preproc[exp_id] = (_dir,preproc_dir)
-			else: continue
+    '''
+    science_lists = np.load('/global/homes/k/kgb0255/package/deepcr/raw_lists_SCIENCE.npy', allow_pickle = True)[()]
+    preproc_dir = '/global/cfs/cdirs/desi/spectro/redux/andes/preproc/'
+    preproc_lists = {}
 
-	np.save('/global/homes/k/kgb0255/packages/deepcr/dark_preproc.npy', np.array(dark_preproc))
+    recent_exps = os.listdir(preproc_dir)
+    length = len(recent_exps)
+    for i, date in enumerate(recent_exps):
+        print(f'{i}/{length}', end = '\r')
+        exp_ids = os.listdir(os.path.join(preproc_dir,str(date)))
+        for exp_id in exp_ids:
+            exp_dir = os.path.join(preproc_dir,str(date),str(exp_id))
+            files = os.listdir(exp_dir)
+            preproc_files = []
+            for f in files:
+                if 'preproc' in f:
+                    preproc_files.append(os.path.join(exp_dir,f))
 
-	return None
+            preproc_lists[exp_id] = preproc_files
+
+    np.save(f'/global/homes/k/kgb0255/packages/deepcr/preproc_lists.npy',np.array(preproc_lists))
+
+    return None
 
 
 if __name__== '__main__':
-	update = sys.argv[1]
-	query_type = sys.argv[2]
+    update = sys.argv[1] == 'True'
+    search_type = sys.argv[2]
 
-	if update:
-		get_dark()
-		get_dark_preproc()
-	else: pass
-	
-	if query_type == 'dark':
-		get_dark()
-	
-	elif query_type == 'dark_preproc':
-		f_exist = os.isfile('/global/homes/k/kgb0255/packages/deepcr/dark_lists.npy')
-		if f_exist:
-			get_dark_preproc()
-		else: 
-			get_dark()
-			get_dark_preproc()
+    if update:
+        get_raw('DARK')
+        get_raw('SCIENCE')
+        get_preproc()
+    
+    else:
+        if search_type == 'dark':
+            get_raw('DARK')
+
+        elif search_type == 'raw_science':
+            get_raw('SCIENCE')
+
+        elif search_type == 'preproc_science':
+            f_exist = os.path.isfile('/global/homes/k/kgb0255/packages/deepcr/raw_lists_SCIENCE.npy')
+            if f_exist:
+                get_preproc()
+            else:
+                get_raw('SCIENCE')
+                get_preproc()
 
 
 
